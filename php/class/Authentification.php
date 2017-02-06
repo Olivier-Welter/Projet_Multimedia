2a23c5c62af4bc1be0b17f653b997e64126da2df
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-06 16:19:42 +0100

    auth

--- php/class/Authentification.php
+++ php/class/Authentification.php
@@ -1,12 +1,18 @@
 <?php
 
-class Authentification extends Session
+class Authentification 
 {
+    private $session;
+    
+    function __construct($session){
+        $this->session = $session;
+    }
+    
     function isAuth(){
         
     }
     
-    function checkUser($user,$pass){
+    function checkUser($login,$passwd){
         
     }
     
@@ -14,3 +20,35 @@ class Authentification extends Session
         
     }
 }
+
+/*class Auth{
+
+    private $session; // variable de la session
+
+    public function __construct($session){
+        $this->session = $session;
+    }
+
+    public function connect($user){
+        $this->session->write('auth', $user);
+    }
+
+    public function login($db, $login, $passwd){
+        $user = $db->query('SELECT * FROM users WHERE (username = :username) AND confirmed_at IS NOT NULL', ['username' => $username])->fetch();
+        if(password_verify($password, $user->password)){
+            $this->connect($user);
+            return $user;
+        }else{
+            return false;
+        }
+    }
+
+    public function restrict(){
+        if(!$this->session->read('auth')){
+            $this->session->setFlash('danger', "Vous n'avez pas le droit d'accéder à cette page");
+            header('Location: login.php');
+            exit();
+        }
+    }
+}
+*/
