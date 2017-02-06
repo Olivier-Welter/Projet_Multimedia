f1a80dce89af3487c9330fa071eed56bd235c922
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-02-06 16:34:47 +0100

    test requete SQL

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $dbc = new ConnectDB(
         'maemoon.com.mysql',
         'maemoon_com',
         'TotoIsNotTiti');
-
+//$dbc->dbQuery('SELECT * FROM users');
 // $dbc = new ConnectDB(
 //         'mysql',
 //         'mediabdd',
