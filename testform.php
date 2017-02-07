ba8d13a065ac63e026e513b4a48336cd5b99bcf5
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-02-07 15:09:53 +0100

    ow

--- testform.php
+++ testform.php
@@ -7,18 +7,29 @@ $s->setMaxHops(1000);
 $s->setMaxAge(10000); 
 if ($s->start()) { 
 
-    // la variable aléatoire qui sera stockée en session 
-    //printf("<h2>Il reste <em>%s</em> accès.</h2>\n", $s->getRemainingHops());
-    //printf("<h2>Il reste <em>%s</em> secondes.</h2>\n", $s->getRemainingTime()); 
-    //printf("<p>voici la variable <var>test</var> précédémment enregistrée en session : <em>%s</em>.</p>\n", $s->get('test')); 
-    printf("<p>Session ID : <em>%s</em>.</p>\n", $newval); 
-    $s->set('test', $newval); 
-    // stockage de la variable "test" en session 
+
 } 
 else { 
     print("<p>Session terminée.</p>\n"); 
     }
-//=============================================	
+
+//==================================================	
+	$dbc = new ConnectDB(
+        'mysql',
+        'maemoon_com',
+        'localhost',
+        'root',
+        '');
+		
+		//$dbc->dbQuery('SELECT * FROM users');
+// $dbc = new ConnectDB(
+//         'mysql',
+//         'mediabdd',
+//         'localhost',
+//         'root',
+//         '');
+		
+//==================================================	
 	
 $form = new FormRecherche();
 echo $form;
