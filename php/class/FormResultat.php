b78fc580d77240e8bd42eff39c4a4ede3a71db57
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-07 12:49:14 +0100

    formResult v0.3

--- php/class/FormResultat.php
+++ php/class/FormResultat.php
@@ -1,8 +1,8 @@
 <?php 
-include("ConnectDB.php");
-class FormResultat extends ConnectDB{
+
+//class FormResultat extends BaseForm{
     
-    private $auteur;
+   /* private $auteur;
     private $descr;
     private $type[];
     
@@ -12,7 +12,7 @@ class FormResultat extends ConnectDB{
     il affiche le résultat de la recherche en fonction de sa date 
     de la plus récente à la moins récente avec la vignette, 
     la description et l'élément recherché 
-    **/
+    **
     function __construct ($auteur,$descr,$type[]){
         $this->auteur=$auteur;
         $this->descr=$descr;
@@ -20,37 +20,34 @@ class FormResultat extends ConnectDB{
         
     }
     
-    
-    function rechercheResult(){
-        $query = 'SELECT date,description,login FROM users,datas WHERE auteur_id.datas = users_id.users WHERE date DESC';
-    }
     function afficherResult(){
-         if ($result = $mysqli->query($query)) {
+        return  $this->auteur." ".$this->descr;
+    }*/
+    
+    $mysql = new PDO("mysql:host=localhost", "maemoon_com", "root", "");
+    if (mysqli_connect_errno()) {
+    printf("Échec de la connexion : %s\n", $mysql->connect_error);
+    exit();
+}
 
-    //Récupère un tableau associatif 
+    $query = 'SELECT date,description,login FROM users,datas WHERE auteur_id.datas = users_id.users WHERE date DESC';
+    
+    if ($result = $mysql->query($query)) {
+
+    /* Récupère un tableau associatif */
     while ($row = $result->fetch_assoc()) {
         echo $row["description"]." ".$row["login"]." ".$row["date"];
     }
 
-    //Libération des résultats 
-    $result->free();
-    }
-    
-}
-
-   /* 
-    
-    if ($result = $mysqli->query($query)) {
-
-    /* Récupère un tableau associatif 
-    while ($row = $result->fetch_assoc()) {
-        echo $row["description"]." ".$row["login"]." ".$row["date"];
-    }
-
-    //Libération des résultats 
+    /* Libération des résultats */
     $result->free();
 }
 
-//Fermeture de la connexion 
-$mysql->close();*/
-}
+/* Fermeture de la connexion */
+$mysql->close();
+//}
+
+
+
+//Michel : un peu dur  tres vite , penser à abandonner
+//Bastien : il se sent tres bien
