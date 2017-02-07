4fef6a0fabcd749923ca1c0e4adf82404e389a44
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-07 11:44:58 +0100

    modif class v0.2

--- php/class/FormResultat.php
+++ php/class/FormResultat.php
@@ -14,5 +14,25 @@ class FormResultat extends BaseForm{
     la description et l'élément recherché 
     **/
     
+    $mysql = new PDO("mysql:host=localhost", "maemon_com", "root", "");
+    if (mysqli_connect_errno()) {
+    printf("Échec de la connexion : %s\n", $mysql->connect_error);
+    exit();
+}
+
+    $query = 'SELECT date,description,nom FROM users,datas WHERE auteur_id.datas = users_id.users WHERE date DESC';
     
+    if ($result = $mysqli->query($query)) {
+
+    /* Récupère un tableau associatif */
+    while ($row = $result->fetch_assoc()) {
+        echo $row["description"]." ".$row["nom"]." ".$row["date"];
+    }
+
+    /* Libération des résultats */
+    $result->free();
+}
+
+/* Fermeture de la connexion */
+$mysql->close();
 }
