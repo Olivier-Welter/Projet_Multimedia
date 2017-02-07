92a31c26291ab3d25dc095f7c88dce929fb3c324
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-07 11:22:22 +0100

    modif class result

---
+++ php/class/FormResultat.php
@@ -0,0 +1,18 @@
+<?php 
+
+class FormResultat extends BaseForm{
+    
+    private $auteur;
+    private $descr;
+    private $type[];
+    
+    /**
+    * on informe les inputs dans la recherche 
+    quand on click sur valider
+    il affiche le résultat de la recherche en fonction de sa date 
+    de la plus récente à la moins récente avec la vignette, 
+    la description et l'élément recherché 
+    **/
+    
+    
+}
