4d490bbcbc8cdb01498ade0fd0c56152d344ac21
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-02-07 16:49:51 +0100

    ow

--- upload.php
+++ upload.php
@@ -3,8 +3,8 @@
 require_once('php/autoload.php');
 //===================SESSION==========================
 $s = Session::getInstance('session_non_auth'); 
-
 $s->setMaxAge(10000); 
+
 if ($s->start()) { 
  echo "session_id : ".session_id().'<br>';
 
@@ -13,24 +13,6 @@ else {
     print("<p>Session terminée.</p>\n"); 
     }
 
-//==================================================	
-	$dbc = new ConnectDB(
-        'mysql',
-        'maemoon_com',
-        'localhost',
-        'root',
-        '');
-		
-		//$dbc->dbQuery('SELECT * FROM users');
-// $dbc = new ConnectDB(
-//         'mysql',
-//         'mediabdd',
-//         'localhost',
-//         'root',
-//         '');
-		
-//==================================================	
-	
 
 
 $form = new FormUpload();
