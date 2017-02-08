2285c8a81f02bd4a964e629e59b65726d399966a
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-08 12:30:21 +0100

    v 0.6.4

--- php/class/FormResultat.php
+++ php/class/FormResultat.php
@@ -1,6 +1,6 @@
 <?php 
 
-/*class FormResultat {
+class FormResultat {
     
     private $query;
     private $result;
@@ -10,19 +10,20 @@
     il affiche le résultat de la recherche en fonction de sa date 
     de la plus récente à la moins récente avec la vignette, 
     la description et l'élément recherché 
-    *
+    **/
     function __construct ($query){
-        $this->query=$query;
-     $dbc = new ConnectDB(
+       // $this->query=$query;
+         $dbc = new ConnectDB(
                             'mysql',
                             'maemoon_com',
                             'localhost',
-                            'root',
-                            '');
-       
+                            'root','');
+    
+         $this->result=$dbc->dbQuery($query);
+        
     }
     
-    function afficherPremFois(){ // affichage des derniers rajouts quand l'utilisateur est là pour la première fois
+    /*function afficherPremFois(){ // affichage des derniers rajouts quand l'utilisateur est là pour la première fois
          $query=mysql_query("SELECT auteur_id,description,date FROM datas WHERE date LIMIT 5");
     }
     
@@ -30,6 +31,8 @@
     function afficherResult(){ // affichage des résultats demandés
         $query="SELECT auteur_id,description,date FROM datas WHERE date";
        echo $query;
+    }*/
+    function __toString(){
+        return "(".$this->result.")";
     }
-    
-}*/
+}
