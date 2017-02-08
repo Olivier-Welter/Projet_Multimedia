d7c5be16355445e385f5f58eef2e8c3504ee0b21
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-02-08 10:58:16 +0100

    gestion des pacges d'affichage

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo <<< EOT
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Bibliothèque multimédia</title>
+		<link rel="stylesheet" href="./css/default.css">
+	</head>
+EOT;
--- upload.php
+++ upload.php
@@ -9,14 +9,17 @@ if ($s->start()) {
 } else {
     print("<p>Session terminée.</p>\n"); 
 }
+echo <<< EOT
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Bibliothèque multimédia</title>
+		<link rel="stylesheet" href="./css/default.css">
+	</head>
+EOT;
 ?>
-    <!DOCTYPE html>
-
-    <head>
-        <meta charset='utf-8' />
-        <link href="css/default.css" rel="stylesheet" />
-        <title>Upload de fichiers</title>
-    </head>
 
     <body>
         <header>
