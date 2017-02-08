aa880e327248aa4d6bae1e58d4cb01965027b23e
Alex3132 <alexis.zueras.pro@gmail.com>
2017-02-08 15:57:43 +0100

    suppression des com inutiles

--- php/class/FormResultat.php
+++ php/class/FormResultat.php
@@ -12,7 +12,7 @@ class FormResultat {
     la description et l'élément recherché 
     **/
     function __construct ($query){
-       // $this->query=$query;
+
          $dbc = new ConnectDB(
                             'mysql',
                             'maemoon_com',
@@ -22,16 +22,7 @@ class FormResultat {
          $this->result=$dbc->dbQuery($query);
         
     }
-    
-    /*function afficherPremFois(){ // affichage des derniers rajouts quand l'utilisateur est là pour la première fois
-         $query=mysql_query("SELECT auteur_id,description,date FROM datas WHERE date LIMIT 5");
-    }
-    
-    
-    function afficherResult(){ // affichage des résultats demandés
-        $query="SELECT auteur_id,description,date FROM datas WHERE date";
-       echo $query;
-    }*/
+
     function __toString(){
         $str = ""; 
         foreach ($this->result as $key => $value){
@@ -43,17 +34,17 @@ class FormResultat {
                            case("image/svg"):
                              case ('image/png'):
                             case ('image/gif'):
-                            $str=$str."<img src='$v' class='vignette'/>";
+                            $str=$str."<img src='$v' class='vignette'/><br/>";
                            break;
                            case("audio/ogg"):
                            case("audio/mp3"): 
                             $str=$str."<audio controls> 
-                           <source src='$v' type='".$value['mime_type']."' class='vignette'/></audio>";
+                           <source src='$v' type='".$value['mime_type']."' class='vignette'/></audio><br/>";
                            break;
                            case("video/webm"):
                            case("video/mp4"):
                             $str=$str."<video controls> 
-                           <source src='$v' type='".$value['mime_type']."' class='vignette'/></video>";
+                           <source src='$v' type='".$value['mime_type']."' class='vignette'/></video><br/>";
                            break;
                    }
         
