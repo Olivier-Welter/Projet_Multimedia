0a8249beee7fdd21c3dfa6fdc96f07121d362b9d
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-02-08 16:10:42 +0100

    ow

--- php/class/FormAuth.php
+++ php/class/FormAuth.php
@@ -4,7 +4,7 @@ class FormAuth extends BaseForm {
     public function __construct(){
         $this->formAttr = ['action'=>'#', 'method'=>'post', 'name'=>'authform'];
         $this->addElem('input', ['name' => "login", 'type' => 'text', 'placeholder'=>'login'], 'Utilisateur : ');
-        $this->addElem('input', ['name' => "passwd", 'type' => 'text', 'placeholder'=>'passwd'], 'Mot de passe : ');
+        $this->addElem('input', ['name' => "passwd", 'type' => 'password', 'placeholder'=>'passwd'], 'Mot de passe : ');
 
         $this->addElem('input', ['name' => "auth", 'type' => 'submit', 'value'=>'Authentification']);
 
