dc8743c603007b277a8b4bc185204ca47ce7720a
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-02-08 10:35:49 +0100

    ow

---
+++ recherche.php
@@ -0,0 +1,33 @@
+<?php
+require_once('php/autoload.php');
+//===================SESSION==========================
+$s = Session::getInstance('session_non_auth'); 
+$s->setMaxAge(10000); 
+
+if ($s->start()) { 
+ echo "session_id : ".session_id().'<br>';
+} else {
+    print("<p>Session terminée.</p>\n"); 
+}
+?>
+    <!DOCTYPE html>
+
+    <head>
+        <meta charset='utf-8' />
+        <link href="css/default.css" rel="stylesheet" />
+        <title>Recherche de fichiers</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Projet PHP Objet</h1>
+        </header>
+        <div class="form">
+            <h2>Rechercher un fichier</h2>
+            <?php
+            $form = new FormRecherche();
+            echo $form;
+
+        ?>
+        </div>
+    </body>
