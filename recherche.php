1d8190be74d5d7fcf48acdb469850f75136ef3c9
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-02-08 15:53:28 +0100

    ow

--- recherche.php
+++ recherche.php
@@ -1,11 +1,11 @@
 <?php
 require_once('php/autoload.php');
 //===================SESSION==========================
-$s = Session::getInstance('session_non_auth'); 
+$s = Session::getInstance(); 
 $s->setMaxAge(10000); 
 
 if ($s->start()) { 
- echo "session_id : ".session_id().'<br>';
+
 } else {
     print("<p>Session terminée.</p>\n"); 
 }
