13d568a783ddfd236802410ad4c7b7a5d0f0433e
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-02-08 11:34:46 +0100

    mise en place des pages du site

--- authentification.php
+++ authentification.php
@@ -11,7 +11,7 @@ echo <<< EOT
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Bibliothèque multimédia</title>
+		<title>Connexion - Bibliothèque multimédia</title>
 		<link rel="stylesheet" href="./css/default.css">
 	</head>
 	<body>
--- upload.php
+++ upload.php
@@ -1,64 +1,26 @@
 <?php
-require_once('php/autoload.php');
-//===================SESSION==========================
-$s = Session::getInstance('session_non_auth'); 
-$s->setMaxAge(10000); 
 
-if ($s->start()) { 
- echo "session_id : ".session_id().'<br>';
-} else {
-    print("<p>Session terminée.</p>\n"); 
-}
+require_once('php/autoload.php');
+$s = Session::getInstance('session_non_auth');
+$s->setMaxAge(10000); 
+$s->start();
+
 echo <<< EOT
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Bibliothèque multimédia</title>
+		<title>Charger - Bibliothèque multimédia</title>
 		<link rel="stylesheet" href="./css/default.css">
 	</head>
+	<body>
+
 EOT;
-?>
+echo '<header>'."\n";
+echo '</header>'."\n";
 
-    <body>
-        <header>
-            <h1>Projet PHP Objet</h1>
-        </header>
-		<div class="form">
-			<?php
-			
-				$dbc = new ConnectDB(
-				'mysql',
-				'maemoon_com',
-				'localhost',
-				'root',
-				'');
-				
-				$faut = new FormAuth();
-				echo $faut;
-				//==================================================	
-				$resultSet = $dbc->dbQuery('SELECT * FROM users where login=\''.$_POST['login'].'\' and passwd = \''.$_POST['passwd'].'\'');
-if($s->get('status')==0){
-				if(count($resultSet)==1){
-				$s->set('login',$_POST['login']);
-				$s->set('status',1);
-				}
-				else{
-				$s->set('login','');
-				$s->set('status',0);	
-				}
-
-				print_r($resultSet);
-				var_dump($_SESSION);
-}
-			?>
-		</div>		
-        <div class="form">
-            <h2>Déposer un fichier</h2>
-            <?php
-            $form = new FormUpload();
-            echo $form;
-        ?>
-        </div>
+echo <<< EOF
     </body>
+</html>
+EOF;
