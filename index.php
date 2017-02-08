005e83fce48132d09185b8476aaf300fed645ca6
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-02-08 15:52:46 +0100

    mise en place des formulaires

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ $s = Session::getInstance('session_non_auth');
 $s->setMaxAge(10000); 
 $s->start();
 
+$form_Auth = new FormAuth();
+$form_Rech = new FormRecherche();
+$form_Resul = new FormResultat($form_Rech->search());
+
 echo <<< EOT
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
@@ -18,8 +22,17 @@ echo <<< EOT
 
 EOT;
 echo '<header>'."\n";
-echo '<p>PROJET PHP Objet - Bibliothèque Multim&eacute;dia</p>';
+echo '<p>PROJET PHP Objet - Bibliothèque Multim&eacute;dia</p>'."\n";
+echo $form_Auth;
 echo '</header>'."\n";
+
+echo '<div>'."\n";
+echo $form_Rech;
+echo '</div>'."\n";
+
+echo '<div>'."\n";
+echo $form_Resul;
+echo '</div>'."\n";
 
 echo <<< EOF
     </body>
