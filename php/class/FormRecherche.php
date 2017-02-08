fa2831b8363611c10520fcd568456cbf11c28112
cbessou <claire.bessou@laposte.net>
2017-02-08 17:09:39 +0100

    génération de la requête pour la recherche de fichiers

--- php/class/FormRecherche.php
+++ php/class/FormRecherche.php
@@ -20,18 +20,51 @@ class FormRecherche extends BaseForm {
                 if ($val == 'video') $this->champsAttr[4]['checked'] = 'checked';
                 }
             }
+        }else{
+            $this->champsAttr[2]['checked'] = 'checked';
+            $this->champsAttr[3]['checked'] = 'checked';
+            $this->champsAttr[4]['checked'] = 'checked';
         }
     }
 
     /*public function __toString(){ $str = ''; foreach($this->champsAttr as $key => $val){ $in = new HTMLBalises($this->champsType[$key], $val, 'test'); if($this->champsLabel[$key] !== ''){ $elem = new HTMLBalises('label', [], $this->champsLabel[$key].$in); $str.=' '.$elem; }else{ $str.=' '.$in; } $str.=' '.new HTMLBalises('br'); } $form = new HTMLBalises('form', ['action'=>'#', 'method'=>'post', 'name'=>'searchform'], $str); return "$form"; }*/
 
 
+    /*
+    Pas de paramètres
+    Retourne une chaine de caractères correspondant à la requête qui doit être envoyée à la base de données
+    */
     public function search(){
-        $query = "SELECT auteur_id,description,date FROM datas WHERE date LIMIT 5";
+        //si on a pas lancé de recherche on utilise la requête par défaut
+        $query = "SELECT * FROM datas ORDER BY date LIMIT 5";
+        //si on a récupéré des valeurs on les utilise pour la recherche
         if(isset($this->champsAttr[0]['value'])){
-            $query = "SELECT * from datas";
+            //recheche en fonction du type de fichiers
+            $query = "SELECT * from datas WHERE (";
+            if (isset($this->champsAttr[2]['checked'])){
+                $query.="mime_type LIKE 'image/%'";
+                if (isset($this->champsAttr[3]['checked'])){
+                    $query.=" OR mime_type LIKE 'audio/%'";
+                }
+                if (isset($this->champsAttr[4]['checked'])){
+                    $query.=" OR mime_type LIKE 'video/%'";
+                }
+            }else{
+                if (isset($this->champsAttr[3]['checked'])){
+                    $query.="mime_type LIKE 'audio/%'";
+                    if (isset($this->champsAttr[4]['checked'])){
+                        $query.=" OR mime_type LIKE 'video/%'";
+                    }
+                }else{
+                    if (isset($this->champsAttr[4]['checked'])){
+                        $query.="mime_type LIKE 'video/%'";
+                    }
+                }
+            }
+            $query.=")";
+            //recherche de l'auteur et de la description
             if($this->champsAttr[0]['value']!=''){
-                $query .= " WHERE auteur_id = '".$this->champsAttr[0]['value']."'";
+                $query .= " AND auteur_id LIKE '%".$this->champsAttr[0]['value']."%'";
                 if($this->champsAttr[1]['value']!=''){
                     $query.=" AND description LIKE '%".$this->champsAttr[1]['value']."%'";
                 }
